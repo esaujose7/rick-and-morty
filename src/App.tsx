24fb6f3232ba25920fc1319ef2170cc318b12156
Esau Murillo <emurillo@artear.com>
2020-05-30 22:18:27 -0300

    trying useQuery and typing my first promise yay

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { useQuery } from "react-query";
 import './App.css';
 
+interface RickAndMortyAPIResponse {
+  characters: string;
+  episodes: string;
+  location: string;
+}
+
+const getPosts = async (): Promise<RickAndMortyAPIResponse> => {
+  const response = await fetch(process.env.REACT_APP_API_BASE_URL);
+  if (response.ok) {
+    const data = await response.json();
+    return data;
+  }
+  const { status: httpCode, statusText, url } = response;
+  throw new Error(`${httpCode} | ${statusText} ${url}`);
+};
+
 function App() {
+  const { status, data, error } = useQuery('base', getPosts);
+  console.log(status, data, error);
+
   return (
     <div className="App">
     </div>
